22abee07290ba4195197ac9f24027df5990dc57d
yohikazu-shibuya <ys0979@gmail.com>
2021-04-24 14:47:23 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -4,9 +4,9 @@ function post (){
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
+    const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
-    XHR.response = "json";
+    XHR.responseType = "json";
     XHR.send(formData);
   });
 }
