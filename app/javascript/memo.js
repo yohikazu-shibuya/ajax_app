c37efc79c8ffabbb0f2e89d9790c3aa91ec7e9ae
yohikazu-shibuya <ys0979@gmail.com>
2021-04-24 12:36:39 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,14 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.response = "json";
+    XHR.send(formData);
+  });
+}
+
+window.addEventListener('load', post);
